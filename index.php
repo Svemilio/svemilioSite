962a757360660a5c6a867ef539f8cdef20c9e2ba
Emilio <emiliosava65@gmail.com>
2024-01-11 13:03:34 +0100

    aggiunta imagini modifica style e php

--- index.php
+++ index.php
@@ -8,13 +8,19 @@
             <h1>I'M</h1>
             <h1>Emilio</h1>
         </div>
+        <img src = "Img/Emilio.png" alt = "me" style = "margin: 100px;">
     
-        <h4 style = "margin-left : 8.5em;">Sono uno studente di informatica,
+        <h4 style = "margin-left : 8.5em; font-family:'Heebo'">Sono uno studente di informatica,
             <br>alle medie girando su youtune scopri il modding
             <br>che consisteva nel modificare uno smartphone in modo radicale
             <br>aggiugengo cose che la casa madre non metteva a disposizione
             <br>poi iniziai a sviluppare dei piccoli videogiochi e adesso
             <br>voglio dedicarmi allo sviluppo mobile e continuare ad usare Unity
         </h4>
+        <br><hr style = "border-color:black; margin:3px;">
+        <div class = "title_my_project";>
+            <h2>I miei progetti</h2>
+        </div>
+        
     </body>
 </html>
